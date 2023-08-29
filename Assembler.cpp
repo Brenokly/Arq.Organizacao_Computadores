f82ad92c170e28e45e7607ace4e1cbcd7eb4818e
Afonso017 <afonsosd016@gmail.com>
2023-08-29 20:56:01 -0300

    Att: Versão final

--- Assembler.cpp
+++ Assembler.cpp
@@ -26,9 +26,9 @@ label* labels;
 int tam;
 
 string codRegistrador(string r, registradores* regis);
-bitset<32> convertTypeR(string linha, instruction * ins, registradores * regis);
-bitset<32> convertTypeJ(string str, instruction * ins, registradores * regis);
-bitset<32> convertTypeI(string str, instruction * ins, registradores * regis);
+bitset<32> convertTypeR(string linha, instruction* ins, registradores* regis);
+bitset<32> convertTypeJ(string str, instruction* ins, registradores* regis);
+bitset<32> convertTypeI(string str, instruction* ins, registradores* regis);
 
 int main() {
     registradores* regis = new registradores[32]{
@@ -39,25 +39,27 @@ int main() {
     {"$s0", "$16", "10000"}, {"$s1", "$17", "10001"}, {"$s2", "$18", "10010"}, {"$s3", "$19", "10011"},
     {"$s4", "$20", "10100"}, {"$s5", "$21", "10101"}, {"$s6", "$22", "10110"}, {"$s7", "$23", "10111"},
     {"$t8", "$24", "11000"}, {"$t9", "$25", "11001"}, {"$k0", "$26", "11010"}, {"$k1", "$27", "11011"},
-    {"$gp", "$28", "11100"}, {"$sp", "$29", "11101"}, {"$fp", "$30", "11110"}, {"$ra", "$31", "11111"}};
+    {"$gp", "$28", "11100"}, {"$sp", "$29", "11101"}, {"$fp", "$30", "11110"}, {"$ra", "$31", "11111"}
+    };
 
-	instruction* inst = new instruction[31]{
-    {"beq", convertTypeI, "000100", 0}, {"bne", convertTypeI, "000101", 0},
-    {"addi", convertTypeI, "001000", 0}, {"addiu", convertTypeI, "001001", 0},
-    {"slti", convertTypeI, "001010", 0}, {"sltiu", convertTypeI, "001011", 0},
-    {"andi", convertTypeI, "001100", 0}, {"ori", convertTypeI, "001101", 0},
-    {"lui", convertTypeI, "001111", 0}, {"lw", convertTypeI, "100011", 0},
-    {"sw", convertTypeI, "101011", 0},{"j", convertTypeJ, "000010", 0},
-    {"jal", convertTypeJ, "000011", 0},{"sll", convertTypeR, "000000", 0},
-    {"srl", convertTypeR, "000010", 0}, {"jr", convertTypeR, "001000", 0},
-    {"mfhi", convertTypeR, "010000", 0}, {"mflo", convertTypeR, "010010", 0},
-    {"mult", convertTypeR, "011000", 0}, {"multu", convertTypeR, "011001", 0},
-    {"div", convertTypeR, "011010", 0}, {"divu", convertTypeR, "011011", 0},
-    {"add", convertTypeR, "100000", 0}, {"addu", convertTypeR, "100001", 0},
-    {"sub", convertTypeR, "100010", 0}, {"subu", convertTypeR, "100011", 0},
-    {"and", convertTypeR, "100100", 0}, {"or", convertTypeR, "100101", 0},
-    {"slt", convertTypeR, "101010", 0},{"mul", convertTypeR, "000010", 0},
-    {"sltu", convertTypeR, "101011", 0}};
+    instruction* inst = new instruction[31]{
+    {"beq", convertTypeI, "000100"}, {"bne", convertTypeI, "000101"},
+    {"addi", convertTypeI, "001000"}, {"addiu", convertTypeI, "001001"},
+    {"slti", convertTypeI, "001010"}, {"sltiu", convertTypeI, "001011"},
+    {"andi", convertTypeI, "001100"}, {"ori", convertTypeI, "001101"},
+    {"lui", convertTypeI, "001111"}, {"lw", convertTypeI, "100011"},
+    {"sw", convertTypeI, "101011"},{"j", convertTypeJ, "000010"},
+    {"jal", convertTypeJ, "000011"},{"sll", convertTypeR, "000000"},
+    {"srl", convertTypeR, "000010"}, {"jr", convertTypeR, "001000"},
+    {"mfhi", convertTypeR, "010000"}, {"mflo", convertTypeR, "010010"},
+    {"mult", convertTypeR, "011000"}, {"multu", convertTypeR, "011001"},
+    {"div", convertTypeR, "011010"}, {"divu", convertTypeR, "011011"},
+    {"add", convertTypeR, "100000"}, {"addu", convertTypeR, "100001"},
+    {"sub", convertTypeR, "100010"}, {"subu", convertTypeR, "100011"},
+    {"and", convertTypeR, "100100"}, {"or", convertTypeR, "100101"},
+    {"slt", convertTypeR, "101010"},{"mul", convertTypeR, "000010"},
+    {"sltu", convertTypeR, "101011"}
+    };
 
     ifstream fin;
     fin.open("main.asm");
@@ -70,135 +72,81 @@ int main() {
     labels = new label[50];
 
     // primeira leitura
-    string str{};
+    string str, s;
     int linha{};
-    char c[50];
     while (fin >> str) {
-        if (str.back() == ':') {                // verifica o ultimo caractere
-            str.erase(str.find(str.back()), 1); // apaga ':', se houver
-            labels[tam].name = str;             // guarda a label
-            labels[tam++].address = linha++;      // guarda o endereco da label
+        if (str.back() == ':') {                            // verifica o ultimo caractere
+            labels[tam].name.assign(str, 0, str.find(':')); // guarda a label sem o ':'
+            labels[tam++].address = linha++;                // guarda o endereco da label
         }
-        fin.getline(c, 50);     // pula para a proxima linha
-        fin.clear();            // limpa o buffer
+        getline(fin, s);     // pula para a proxima linha
     }
 
-    /*cout << "Labels: \n";
     for (int i = 0; i < tam; i++) {
         cout << labels[i].address << ' ' << labels[i].name << endl;
     }
-    cout << endl;*/
 
     fin.close();
     fin.open("main.asm");
-    if(!fin.is_open())
-    {
-        cout << "falha ao abrir o arquivo main.asm!" << endl;
-    }
+
     ofstream fout;
     fout.open("assembler.hex");
-    if(!fout.is_open())
-    {
-        cout << "falha ao abrir o arquivo assemble.hex!" << endl;
-    }
     fout << "v2.0 raw\n";
 
-    bitset<32> codigo;
-	string ins{},r{};
-    int endereco{0}, saida{1};
+    // segunda leitura
+    int endereco{}, saida{ 1 };
+    while (getline(fin, str)) {
+        if (str[0] != '\0') {
+            size_t inv = string::npos;
 
-    while (!fin.eof())
-    {
-        getline(fin,r);
-        // Encontra a posição do caractere '#'
-        size_t posComentario = r.find('#'); 
+            if (str.find('#') != inv)           // apaga o comentario
+                str.erase(str.find('#'));
 
-        if (posComentario != std::string::npos) {
-            r.erase(posComentario); // Remove tudo a partir da posição do caractere '#'
-        }
+            if (str.find(':') != inv)           // apaga a label
+                str.erase(0, str.find(':') + 1);
 
-        // Remove espaços em branco no final da linha
-        size_t posUltimoNaoEspaco = r.find_last_not_of(" \t\r\n");
-        if (posUltimoNaoEspaco != std::string::npos) {
-            r.erase(posUltimoNaoEspaco + 1);
-        }
+            while (str.find('\t') != inv)       // substitui todos os '\t' por ' '
+                str.replace(str.find('\t'), 1, " ");
 
-        // Encontra a posição do caractere ' '
-        size_t posDoisPontos = r.find(':');
-        
-        // Se o caractere ':' for encontrado, remove tudo antes dele, ou seja, remove a label e o ":" se tiver
-        if (posDoisPontos != string::npos) {
-            r.erase(0, posDoisPontos + 1);
+            while (str.front() == ' ')          // remove espacos extras antes da instrucao
+                str.erase(0, 1);
 
-            while (!r.empty() && isspace(r[0])) {
-            r.erase(0, 1);
-            }
-        }
+            while (str.back() == ' ')           // remove espacos extras depois da instrucao
+                str.pop_back();
 
-        while (r[0] == '.'|| r.empty() && !fin.eof())
-        {
-            getline(fin,r);
-
-            // Encontra a posição do caractere '#'
-            size_t posComentario = r.find('#'); 
-
-            if (posComentario != std::string::npos) {
-                r.erase(posComentario); // Remove tudo a partir da posição do caractere '#'
-            }
-
-            // Remove espaços em branco no final da linha
-            size_t posUltimoNaoEspaco = r.find_last_not_of(" \t\r\n");
-            if (posUltimoNaoEspaco != std::string::npos) {
-                r.erase(posUltimoNaoEspaco + 1);
-            }
-
-            // Encontra a posição do caractere ' '
-            size_t posDoisPontos = r.find(':');
-            
-            // Se o caractere ':' for encontrado, remove tudo antes dele, ou seja, remove a label e o ":" se tiver
-            if (posDoisPontos != string::npos) {
-                r.erase(0, posDoisPontos + 1);
-
-                while (!r.empty() && isspace(r[0])) {
-                r.erase(0, 1);
+            if (str != "") {                     // apos todas as modificacoes, verifica se a string esta vazia
+                s.assign(str, 0, str.find(' ')); // atribui nome da instrucao para s
+                bitset<32> codigo;               // codigo binario de 32 bits da instrucao
+                for (int i = 0; i < 39; i++) {   // laco para buscar a instrucao no vetor de instrucoes
+                    if (inst[i].name == s) {
+                        inst[i].endereco = ++endereco; // quando achar a instrucao, atribui o endereco atual
+                        codigo = inst[i].type(str, &inst[i], regis); // chama a funcao apontada pela instrucao
+                        fout.width(8);
+                        fout.fill('0');
+                        if (saida++ % 4 == 0)   // escreve de 4 em 4 valores hexa no arquivo de saida
+                            fout << hex << codigo.to_ullong() << endl;
+                        else
+                            fout << hex << codigo.to_ullong() << ' ';
+                        i = 39;
+                    }
                 }
             }
         }
-
-        ins.assign(r, 0, r.find(' '));
-
-        for (int i = 0; i < 31; i++) {
-            if (inst[i].name == ins) {
-                codigo = inst[i].type(r, &inst[i], regis);
-                inst[i].endereco = endereco++;
-
-                fout.width(8);
-                fout.fill('0');
-                if (saida++ % 4 == 0)
-                       fout << hex << codigo.to_ullong() << endl;
-                else
-                    fout << hex << codigo.to_ullong() << ' ';
-
-                i = 31;
-            }
-        }
     }
-
     fout << endl;
     fin.close();
     fout.close();
     delete[] regis;
     delete[] inst;
     delete[] labels;
-
-    return 0;
 }
-bitset<32> convertTypeR(string linha, instruction * ins, registradores * regis)
+
+bitset<32> convertTypeR(string linha, instruction* ins, registradores* regis)
 {
-    string binario{}, constante{}, rs{}, rt{}, rd{}, nome{ ins->name },funct{ins->opcode};
+    string binario{}, constante{}, rs{}, rt{}, rd{}, nome{ ins->name }, funct{ ins->opcode };
     bitset<6> opcode(0);
     bitset<6> opcodeMul(28);
-    
+
     size_t pos = linha.find(',');
 
     // Enquanto houver vírgulas na string, remove-as
@@ -209,54 +157,54 @@ bitset<32> convertTypeR(string linha, instruction * ins, registradores * regis)
 
     // Encontra a posição do caractere ' '
     size_t vazio = linha.find(' ');
-    
+
     // Se o caractere ':' for encontrado, remove tudo antes dele
     if (vazio != string::npos) {
         linha.erase(0, vazio + 1);
 
         while (!linha.empty() && isspace(linha[0])) {
-        linha.erase(0, 1);
+            linha.erase(0, 1);
         }
     }
 
-	for (int j = 0; linha[j]; j++){
-		if (isdigit(linha[j]) && linha[j - 1] != '$' && linha[j - 2] != '$'){ // evita de pegar constante de instruções que tem número mesmo o tipo R não tendo
+    for (int j = 0; linha[j]; j++) {
+        if (isdigit(linha[j]) && linha[j - 1] != '$' && linha[j - 2] != '$') { // evita de pegar constante de instruções que tem número mesmo o tipo R não tendo
             constante = linha[j];
             linha[j] = '\0';
 
-			if (isdigit(linha[j + 1]))
-			{
-				constante += linha[j + 1]; // verifica se o próximo também não é uma constante caso tenha tipo 14
-                linha[j] = '\0';    
-				break;	
-			}
-			break;
-		}
-	}
+            if (isdigit(linha[j + 1]))
+            {
+                constante += linha[j + 1]; // verifica se o próximo também não é uma constante caso tenha tipo 14
+                linha[j] = '\0';
+                break;
+            }
+            break;
+        }
+    }
 
-    if (nome != "jr" && nome != "mult" && nome != "multu" && nome != "div" && nome != "divu"){
+    if (nome != "jr" && nome != "mult" && nome != "multu" && nome != "div" && nome != "divu") {
 
         rd.assign(linha, 0, linha.find(' '));
 
-        linha.erase(0, linha.find(' ')+1); 
-
-	    while (!linha.empty() && isspace(linha[0])) {
-        linha.erase(0, 1);
-	    }
-    }
-    
-    if (nome != "mfhi" && nome != "mflo" && nome != "sll" && nome != "srl"){
-        rs.assign(linha, 0, linha.find(' '));
-
-        linha.erase(0, linha.find(' ')+1); 
+        linha.erase(0, linha.find(' ') + 1);
 
         while (!linha.empty() && isspace(linha[0])) {
-        linha.erase(0, 1);
-        }      
+            linha.erase(0, 1);
+        }
     }
 
-    if (nome != "jr" && nome != "mfhi" && nome != "mflo"){
-        rt.assign(linha, 0, linha.find(' '));    
+    if (nome != "mfhi" && nome != "mflo" && nome != "sll" && nome != "srl") {
+        rs.assign(linha, 0, linha.find(' '));
+
+        linha.erase(0, linha.find(' ') + 1);
+
+        while (!linha.empty() && isspace(linha[0])) {
+            linha.erase(0, 1);
+        }
+    }
+
+    if (nome != "jr" && nome != "mfhi" && nome != "mflo") {
+        rt.assign(linha, 0, linha.find(' '));
     }
 
     if (nome == "mul")
@@ -265,41 +213,42 @@ bitset<32> convertTypeR(string linha, instruction * ins, registradores * regis)
     }
     else
     {
-       binario.append(opcode.to_string());
+        binario.append(opcode.to_string());
     }
 
-	if (rs.empty())
-	{
+    if (rs.empty())
+    {
         binario.append("00000");
-		
-	}
-	else{
-		binario.append(codRegistrador(rs, regis));
-	}
 
-	if (rt.empty())
-	{
-		binario.append("00000");
-	}
-	else{
+    }
+    else {
+        binario.append(codRegistrador(rs, regis));
+    }
+
+    if (rt.empty())
+    {
+        binario.append("00000");
+    }
+    else {
         binario.append(codRegistrador(rt, regis));
-	}
-	
-	if (rd.empty())
-	{
-		binario.append("00000");
-	}
-	else{
+    }
+
+    if (rd.empty())
+    {
+        binario.append("00000");
+    }
+    else {
         binario.append(codRegistrador(rd, regis));
-	}
+    }
 
     if (constante.empty()) {
-    bitset<5> immediate(0); 
-    binario.append(immediate.to_string());
-    } else {
-    int immediateValue = stoi(constante);
-    bitset<5> immediate(immediateValue);
-    binario.append(immediate.to_string());
+        bitset<5> immediate(0);
+        binario.append(immediate.to_string());
+    }
+    else {
+        int immediateValue = stoi(constante);
+        bitset<5> immediate(immediateValue);
+        binario.append(immediate.to_string());
     }
 
     binario.append(funct);
@@ -308,85 +257,94 @@ bitset<32> convertTypeR(string linha, instruction * ins, registradores * regis)
 
     return binary;
 }
-bitset<32> convertTypeI(string str, instruction * ins, registradores * regis) {
-    string name{ ins->name }, opcode{ ins->opcode }, rs{}, rt{}, im{};
-    str.erase(0, str.find(' ') + 1);    // apaga o nome da instrucao completa
+
+bitset<32> convertTypeI(string str, instruction* ins, registradores* regis) {
+    string name{ ins->name }, opcode{ ins->opcode }, rs{ "00000" }, rt{ "00000" };
+    str.erase(0, str.find('$'));    // apaga o nome da instrucao completa
 
     // verifica qual instrucao a ser codificada
     if (name == "beq" || name == "bne") {
-        rs.assign(str, 0, str.find(','));
-        str.erase(0, str.find(' ') + 1);
-        rt.assign(str, 0, str.find(','));
-        str.erase(0, str.find(' ') + 1);
-        im = str;
+        rs.assign(str, str.find('$'), str.find(',') - str.find('$'));
+        str.erase(0, str.find(',') + 1);
+        rt.assign(str, str.find('$'), str.find(',') - str.find('$'));
+        str.erase(0, str.find(',') + 1);
     }
     else if (name == "lui") {
-        rt.assign(str, 0, str.find(','));
-        str.erase(0, str.find(' ') + 1);
-        im = "00000";
+        rt.assign(str, str.find('$'), str.find(',') - str.find('$'));
+        str.erase(0, str.find(',') + 1);
     }
     else if (name == "lw" || name == "sw") {
-        rt.assign(str, 0, str.find(','));
-        str.erase(0, str.find(' ') + 1);
-        rs.assign(str, str.find('$'), str.length() - str.find('$') - 1);
+        rt.assign(str, str.find('$'), str.find(',') - str.find('$'));
+        str.erase(0, str.find(',') + 1);
+        rs.assign(str, str.find('$'), str.find(')') - str.find('$'));
         str.erase(str.find('('), str.find(')') + 1);
-        im = str;
     }
     else {
-        rt.assign(str, 0, str.find(','));
-        str.erase(0, str.find(' ') + 1);
-        rs.assign(str, 0, str.find(','));
-        str.erase(0, str.find(' ') + 1);
-        im = str;
+        rt.assign(str, str.find('$'), str.find(',') - str.find('$'));
+        str.erase(0, str.find(',') + 1);
+        rs.assign(str, str.find('$'), str.find(',') - str.find('$'));
+        str.erase(0, str.find(',') + 1);
+    }
+
+    while (str.front() == ' ') {    // elimina os espacos
+        str.erase(0, 1);
     }
 
     bitset<16> immediate;   // declara o valor imediato de 16 bits
-    if (isdigit(im[0])) {   // se o imediato for um digito, converte para binario
-        immediate = stoi(im);
+    if (str.front() == '-' || isdigit(str.front())) {     // converte para inteiro se for um numero
+        immediate = stoi(str);
     }
-    else {    
+    else {      // se for uma label, pega o endereco da label e subtrai do endereco da instrucao atual
         for (int i = 0; i < tam; i++) {
-            if (labels[i].name == im) {
+            if (labels[i].name == str) {
                 immediate = labels[i].address - ins->endereco;
             }
         }
     }
 
-    str.clear();
-    str.append(opcode);
-    str.append(codRegistrador(rs, regis));
-    str.append(codRegistrador(rt, regis));
-    str.append(immediate.to_string());
+    str.clear();                            // limpa string
+    str.append(opcode);                     // concatena o opcode
+    str.append(codRegistrador(rs, regis));  // concatena o endereco do registrador rs
+    str.append(codRegistrador(rt, regis));  // concatena o endereco do registrador rt
+    str.append(immediate.to_string());      // concatena o valor imediato, seja um numero ou endereco de label
 
     bitset<32> bit(str);
     return bit;
 }
 
-bitset<32> convertTypeJ(string str, instruction * ins, registradores * regis) {
-    string endereco{}, opcode{ins->opcode}, pc{"000001"};
-    str.erase(0, str.find(' ') + 1);
+bitset<32> convertTypeJ(string str, instruction* ins, registradores* regis) {
+    string endereco{}, opcode{ ins->opcode }, pc{ "000001" };
+    bitset<20> endLabel;    // endereco da label do jump
+
     endereco.append(opcode);
     endereco.append(pc);
-    bitset<20> endLabel;
-    for (int i = 0; i < tam; i++) {
-        if (labels[i].name == str) {
-            endLabel = labels[i].address;
-        }
+
+    str.erase(0, str.find(' '));
+    while (str.front() == ' ') {    // elimina os espacos
+        str.erase(0, 1);
     }
-    endereco.append(endLabel.to_string());
-    
-    bitset<32> bit(endereco);
+
+    for (int i = 0; i < tam; i++)   // busca a label correspondente e pega seu endereco
+        if (labels[i].name == str)
+            endLabel = labels[i].address;
+
+    endereco.append(endLabel.to_string());  // converte o endereco da label para string e concatena em endereco
+
+    bitset<32> bit(endereco);   // converte string endereco em binario de 32 bits
     return bit;
 }
+
 string codRegistrador(string r, registradores* regis) {
     // procura e retorna o codigo binario do registrador
-    for (int i = 0; i < 32; i++) {
-        if (r == regis[i].nome) {
-            return regis[i].codigo;
-        }
-        else if (r == regis[i].numero) {
-            return regis[i].codigo;
+    if (r != "00000") {
+        for (int i = 0; i < 32; i++) {
+            if (r == regis[i].nome) {   // procura por nome ($t0, $t1...)
+                return regis[i].codigo;
+            }
+            else if (r == regis[i].numero) {    // procura por numero ($8, $9...)
+                return regis[i].codigo;
+            }
         }
     }
-    return "";
+    return r;
 }
