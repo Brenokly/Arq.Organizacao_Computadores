3c7540334d3dc5579a1a3a23bca7dc3aa2577334
Breno Klywer Olegario de Moura <breno.moura@alunos.ufersa.edu.br>
2024-02-13 19:12:26 -0300

    organização

---
+++ BibliotecaString/main.asm
@@ -0,0 +1,270 @@
+# Breno Klywer Olegario de Moura
+# Esse arquivo "main.asm" tem a finalidade apenas de testar as bibliotecas implementadas "string.asm".
+# Se tiver comentários sem sentidos provavelmente porquê eu dei ctrl + c e ctrl + v em algum código que dava para 
+# reaproveitar e esqueci de trocar o comentário.
+
+# Observação, o espaço das strings que são lidas não foi alocado dinâmicamente, então conforme for usando o looping
+# o espaço definido vai encher, então se parar ou der erro nas operações é porquê o espaço encheu.
+# fazer com memória alocada dinâmicamente iria dar muito trabalho.
+.data
+
+destino: .space 1000 # Espaço máximo após muito uso do looping esse espaço encherá
+origem: .space 1000 # Espaço máximo após muito uso do looping esse espaço encherá
+menu: .asciiz "\n[0]Sair\n[1]Strlen\n[2]Strcmp\n[3]Strcat\n[4]Strncat\n[5]Strncpy\nOpção: "
+msg1: .asciiz "\nDigite a string de destino: "
+msg2: .asciiz "\nDigite a string de origem: "
+msg3: .asciiz "\nQuantos caracteres: "
+msg4: .asciiz "Caracteres: "
+msg5: .asciiz "Resultado da operação: "
+
+.text
+
+.globl main
+.include "string.asm" # Incluir o conteúdo do arquivo "string.asm"
+
+main:
+la $a0,menu # carregando o menu
+li $v0, 4 # chamando o sistema para imprimir
+syscall 
+
+li $v0, 5 # chamando o sistema para ler um inteiro
+syscall
+
+beq $v0,0,encerrar # se o usuário digitar 0 encerra o programa.
+beq $v0,1,Funstrlen # se o usuário digitar 1 vai para a função strlen.
+beq $v0,2,Funstrcmp # se o usuário digitar 2 vai para a função strcmp.
+beq $v0,3,Funstrcat # se o usuário digitar 3 vai para a função strcat.
+beq $v0,4,Funstrncat # se o usuário digitar 3 vai para a função strncat.
+beq $v0,5,Funstrncpy # se o usuário digitar 3 vai para a função strncpy.
+
+Funstrlen:
+# 1° teste da função strlen
+
+la $a0, msg1 # carregando o endereço da mensagem 1
+li $v0, 4 # imprimir uma string
+syscall
+
+la $a0, destino # carregando o espaço de memória destinado a string de destino
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall 
+
+la $a0, destino # carregando o endereço da string
+
+move $v0,$0 # limpando o $v0
+
+jal strlen # chama a função strlen
+
+move $s0, $v0 # Move o valor de retorno da função strlen para $s0
+
+la $a0,msg4 # carregando a mensagem
+li $v0, 4 # chamnado o sistema para imprimir
+syscall 
+
+move $a0, $s0 # Move o valor de retorno da função strlen para $a0
+
+addi $v0,$0, 1 # coloca o código de 1 dentro de $v0 para imprimir um inteiro
+#usei o addi só para ficar claro que eu sei usar as instruções padrões não só as pseudos.
+
+syscall # chamando o sistema para imprimir o resultado da função.
+
+j main
+
+#########################################################################################################################
+
+Funstrcmp:
+
+# 2° teste da função strcmp
+
+la $a0, msg1 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall
+
+la $a0, destino # carregando o espaço de memória destinado a string de destino
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+la $a0, msg2 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall 
+
+la $a0, origem # carregando o espaço de memória destinado a string de origem
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+la $a0, destino # carregando o endereço da string
+la $a1, origem # carregando o endereço da string 
+
+add $v0,$0,$0 # limpando o $v0
+
+jal strcmp # chama a função strcmp
+
+move $s0, $v0 # Move o valor de retorno da função strlen para $s0
+
+la $a0,msg5 # carregando a mensagem
+li $v0, 4 # chamnado o sistema para imprimir
+syscall 
+
+move $a0, $s0 # Move o valor de retorno da função strlen para $a0
+
+li $v0, 1  # coloca o código de 1 dentro de $v0 para imprimir um inteiro
+
+syscall # chamando o sistema para imprimir o resultado da função.
+
+j main
+
+#########################################################################################################################
+
+Funstrcat:
+
+# 3° teste da função strcat
+
+la $a0, msg1 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall 
+
+la $a0, destino # carregando o espaço de memória destinado a string de destino
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+ la $a0, msg2 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall  
+
+la $a0, origem # carregando o espaço de memória destinado a string de origem
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+la $a0, destino # carregando o endereço da string
+la $a1, origem # carregando o endereço da string 
+
+add $v0,$0,$0 # limpando o $v0
+
+jal strcat # chama a função strlen
+ 
+la $s0, ($v0) # Move o valor de retorno da função strlen para $s0
+
+la $a0,msg5 # carregando a mensagem
+li $v0, 4 # chamnado o sistema para imprimir
+syscall 
+
+la $a0, ($s0) # Move o valor de retorno da função strlen para $a0
+
+li $v0, 4  # coloca o código de 1 dentro de $v0 para imprimir um inteiro
+
+syscall # chamando o sistema para imprimir o resultado da função.
+
+j main
+
+#########################################################################################################################
+
+Funstrncat:
+
+# 4° teste da função strncat
+
+la $a0, msg1 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall 
+
+la $a0, destino # carregando o espaço de memória destinado a string de destino
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+la $a0, msg2 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall  
+
+la $a0, origem # carregando o espaço de memória destinado a string de origem
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+la $a0, msg3 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall 
+
+li $v0,5 # Coloca o código de leitura de um inteiro
+syscall
+
+move $a2, $v0 # copia o valor lido para $a2 que é, por convenção, um registrador destinado a parâmetros.
+
+la $a0, destino # carregando o endereço da string
+la $a1, origem # carregando o endereço da string
+
+jal strncat # chama a função strlen
+
+la $s0,($v0) # Move o valor de retorno da função strlen para $s0
+
+la $a0,msg5 # carregando a mensagem
+li $v0, 4 # chamnado o sistema para imprimir
+syscall 
+
+la $a0, ($s0) # Move o valor de retorno da função strlen para $a0
+
+li $v0, 4  # coloca o código de 1 dentro de $v0 para imprimir um inteiro
+
+syscall # chamando o sistema para imprimir o resultado da função.
+
+j main
+
+#########################################################################################################################
+
+Funstrncpy:
+
+# 5° teste da função strncpy
+
+la $a0, msg1 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall 
+
+la $a0, destino # carregando o espaço de memória destinado a string de destino
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+la $a0, msg2 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall  
+
+la $a0, origem # carregando o espaço de memória destinado a string de origem
+li $a1,100 # tamanho máximo da string a ser lida
+li $v0, 8 # ler uma string
+syscall
+
+la $a0, msg3 # carregando o endereço da mensagem 
+li $v0, 4 # imprimir uma string
+syscall 
+
+li $v0,5 # Coloca o código de leitura de um inteiro
+syscall
+
+move $a2, $v0 # copia o valor lido para $a2 que é, por convenção, um registrador destinado a parâmetros.
+
+la $a0, destino # carregando o endereço da string
+la $a1, origem # carregando o endereço da string
+
+jal strncpy # chama a função strlen
+
+la $s0,($v0) # Move o valor de retorno da função para $s0
+
+la $a0,msg5 # carregando a mensagem
+li $v0, 4 # chamnado o sistema para imprimir
+syscall 
+
+la $a0, ($s0) # Move o valor de retorno da função para $a0
+
+li $v0, 4  # coloca o código de 1 dentro de $v0 para imprimir um inteiro
+
+syscall # chamando o sistema para imprimir o resultado da função.
+
+j main
+
+encerrar:
+li $v0,10 # encerrar o programa
+syscall
+
---
+++ BibliotecaString/string.asm
@@ -0,0 +1,294 @@
+# Breno Klywer olegario de Moura 
+
+# Este arquivo "string.asm" contém a implementação das 5 funções requisitadas no trabalho.
+# Dentre as funções temos: 
+
+# Strlen -> recebe um ponteiro de uma string e retorna a quantidade de caracteres.
+
+# strcmp -> Compara duas strings e retorna um inteiro. Se for negativo é porquê a primeira string é menor que a segunda
+# se for positivo é porquê a primeira string é maior que a segunda e 0 é porquê elas são idênticas.
+
+# strcat -> Concatena duas strings. A string src ao final da string dest.
+
+# strncat -> contatena uma determinada quantidade da string src no final da string dest.
+
+# strncpy -> Faz a mesma coisa que a strcpy, mas em vez de copiar todos os elementos, copia somente os count primeiros
+# da src para a "dest".
+
+.data
+
+msgErro: .asciiz "\nErro: Você excedeu a quantidade de caracteres que a string possuí"
+
+.text
+
+strlen: # Função para contar os caracteres de uma string que está no registrador $a0, retorna o valor int em $v0.
+addi $sp, $sp,-8 # Desce o ponteiro que aponta para pilha 3 vezes, ou seja, aloca 3 espaços na pilha.
+sw $t0, 4($sp) # Carrega os valores contidos em $s0,$t0 e $t1 para a pilha.
+sw $t1, 0($sp) #
+
+add $v0,$0,$0 # limpa o $v0
+add $t0,$0,$0 # limpa o $t0
+add $t1,$0,$0 # limpa o $t1
+
+loop1: # Loopingzinho
+
+lb $t1, 0($a0) # Carrega o primeiro caracter da string que está em $a0 para $t1
+beq $t1, $0, end0 # Se o caracter que está em $t1 não for o \0, ou seja, 0 ele continua contando.
+beq $t1, 10, end0 # se ele encontrar o \n ele vai para remove
+addi $a0, $a0, 1 # pega o próximo caracter somando 1 ao endereço da string.
+addi $t0, $t0, 1 # Soma 1 ao registrador $t0 que representa quantos caracteres tem na string.
+
+j loop1 # Volta para o strlen para testar o próximo caracter.
+
+remove:
+lb $0,0($t1) # joga o \0 dentro do $t1 na posição do \n.
+#Eu sei que o senhor não pediu para tirar o \n, mas eu optei por tirar porquê não faz sentido deixar ele sendo que temos
+# funções como strcat que concatena duas strings, com ele não fica legal a concatenação.
+
+addi $a0, $a0, 1 # pega o próximo caracter somando 1 ao endereço da string.
+
+j loop1 # Volta para o strlen para testar o próximo caracter.
+
+end0:
+add $v0,$v0,$t0 # move o valor de $t0 para $v0 que é por convenção o registrador de retorno.
+
+lw $t1, 0($sp)  #
+lw $t0, 4($sp)  # Restaura os valores de $t1,$t0,$s0 que foram copiados para a pilha.
+addi $sp, $sp, 8 # Restaura ao ponteiro da pilha para o lugar original.
+
+jr $ra # Volta para quem chamou.
+
+#########################################################################################################################
+
+strcmp:
+addi $sp, $sp,-16 # Desce o ponteiro que aponta para pilha 3 vezes, ou seja, aloca 3 espaços na pilha.
+sw $s0, 12($sp)# 
+sw $t0, 8($sp) # Carrega os valores contidos em $s0,$t0,$t1 e $ra para a pilha.
+sw $t1, 4($sp) #
+sw $ra, 0($sp) #
+
+add $v0,$0,$0 # limpa o $v0
+add $s0,$0,$0 # limpa o $s0
+add $t0,$0,$0 # limpa o $t0
+add $t1,$0,$0 # limpa o $t1
+
+jal strlen # Chama a função strlen
+
+move $t0, $v0 # Move o valor de retorno da função strlen para $t0
+
+add $v0,$0,$0 # limpa o $v0
+
+move $a0,$a1 # Move o endereço da string que está em $a1 para $a0 para cálcular o tamanho dela agora.
+
+jal strlen # Chama a função strlen
+
+move $t1, $v0 # Move o valor de retorno da função strlen para $t1
+
+add $v0,$0,$0 # limpa o $v0
+
+beq $t0,$t1,iguais # Se as duas strings forem iguais pula para o label iguais. Se não forem iguais ele continua.
+
+slt $s0, $t0, $t1 # Se a primeira string for menor que a segunda retorna 1, se não, retorna 0.
+
+beq $s0,1, negativo # Se $s0 for igual a 1 significa que a primeira é menor.
+
+positivo:
+addi $v0,$v0,1 # adiciona 1 ao $v0 siginificando que a primeira string é maior que a segunda.
+j end1
+
+negativo:
+addi $v0,$v0,-1 # adiciona -1 ao $v0 siginificando que a primeira string é menor que a segunda.
+j end1
+
+iguais:
+addi $v0,$v0,0 # adiciona 0 ao $v0 siginificando que as  strings são iguais.
+
+end1:
+lw $ra, 0($sp)
+lw $t1, 4($sp) #
+lw $t0, 8($sp) # Restaura os valores de $t1,$t0 e $s0 que foram copiados para a pilha.
+lw $s0, 12($sp) # 
+addi $sp, $sp, 16 # Restaur ao ponteiro da pilha para o lugar original.
+
+jr $ra # Volta para quem chamou.
+
+
+strcat: # contatena uma string src = fonte a uma string dest = destino.
+addi $sp, $sp,-12 # Desce o ponteiro que aponta para pilha 3 vezes, ou seja, aloca 3 espaços na pilha.
+sw $t0, 8($sp) # Carrega os valores contidos em $s0,$t0,$t1 e $ra para a pilha.
+sw $t1, 4($sp) #
+sw $ra, 0($sp) #
+
+add $v0,$0,$0 # limpa o $v0
+add $s0,$0,$0 # limpa o $s0
+add $t0,$0,$0 # limpa o $t0
+add $t1,$0,$0 # limpa o $t1
+
+# String destino $a0
+# String origem $a1
+
+jal strlen # chamando a função strlen para cálcular o tamanho da string dest = destino
+
+move $t0, $v0 # copiando o tamanho da string destino para $t0
+
+loop2:
+
+lb $t1, 0($a1) # Carrega o primeiro caracter da string que está em $a1 para $t1
+
+sb $t1, 0($a0) # Concateno o caracter da string de origem na string de destino que no caso está em $a0
+
+addi $a0, $a0, 1   # Avança para o próximo byte na string de destino
+addi $a1, $a1, 1   # Avança para o próximo byte na string de origem
+addi $t0,$t0,1     # incrementando o valor de $t0 para saber qual endereço voltar no final da função
+
+beq $t1, $0,end2 # Se o caracter que está em $t1 não for o \0, ou seja, 0 ele continua concatenando.
+j loop2
+
+end2:
+sub $a0,$a0,$t0 # subtraindo $a0 que contem o endereço da string de $t0 para voltar para o início dela
+la $v0,($a0) # carregando o endereço do primeiro caracter da string para retornar.
+lw $ra, 0($sp)  #
+lw $t1, 4($sp)  #
+lw $t0, 8($sp)  # Restaura os valores de $t1,$t0 e $s0 que foram copiados para a pilha.
+addi $sp, $sp, 12 # Restaur ao ponteiro da pilha para o lugar original.
+
+jr $ra # Volta para quem chamou.
+
+
+
+strncat: # contatena uma string src = fonte a uma string dest = destino.
+addi $sp, $sp,-12 # Desce o ponteiro que aponta para pilha 3 vezes, ou seja, aloca 3 espaços na pilha.
+sw $t0, 8($sp) # Carrega os valores contidos em $s0,$t0,$t1 e $ra para a pilha.
+sw $t1, 4($sp) #
+sw $ra, 0($sp) #
+
+add $v0,$0,$0 # limpa o $v0
+add $s0,$0,$0 # limpa o $s0
+add $t0,$0,$0 # limpa o $t0
+add $t1,$0,$0 # limpa o $t1
+
+# String destino $a0
+# String origem $a1
+# count $a2
+
+jal strlen # chamando a função strlen para cálcular o tamanho da string dest = destino
+
+move $t0, $v0 # copiando o tamanho da string destino para $t0
+add $a2,$a2,1 # adicionando um vez a mais no looping para ele adicionar o \0
+loop3:
+
+lb $t1, 0($a1) # Carrega o primeiro caracter da string que está em $a1 para $t1
+
+sb $t1, 0($a0) # Concateno o caracter da string de origem na string de destino que no caso está em $a0
+
+addi $a0, $a0, 1   # Avança para o próximo byte na string de destino
+addi $a1, $a1, 1   # Avança para o próximo byte na string de origem
+addi $t0,$t0,1     # incrementando o valor de $t0 para saber qual endereço voltar no final da função
+addi $t3,$t3,1     # criando um contador para encerrar o laço.
+
+beq $t3, $a2, end3 # Se o caracter que está em $t1 não for o \0, ou seja, 0 ele continua concatenando.
+j loop3
+
+end3:
+sb $0, 0($a0) # colocando o \0 no final.
+sub $a0,$a0,$t0 # subtraindo $a0 que contem o endereço da string de $t0 para voltar para o início dela
+la $v0,($a0) # carregando o endereço do primeiro caracter da string para retornar.
+lw $ra, 0($sp)  #
+lw $t1, 4($sp)  #
+lw $t0, 8($sp)  # Restaura os valores de $t1,$t0 e $s0 que foram copiados para a pilha.
+addi $sp, $sp, 12 # Restaur ao ponteiro da pilha para o lugar original.
+
+jr $ra # Volta para quem chamou.
+
+
+strncpy: # Copia alguns caracteres determinados da string de origem para string destino
+addi $sp, $sp,-16 # Desce o ponteiro que aponta para pilha 3 vezes, ou seja, aloca 3 espaços na pilha.
+sw $s0, 12($sp)# 
+sw $t0, 8($sp) # Carrega os valores contidos em $s0,$t0,$t1 e $ra para a pilha.
+sw $t1, 4($sp) #
+sw $ra, 0($sp) #
+
+add $v0,$0,$0 # limpa o $v0
+add $s0,$0,$0 # limpa o $s0
+add $t0,$0,$0 # limpa o $t0
+add $t1,$0,$0 # limpa o $t1
+
+# string origem $a1
+# string destino $a0
+# count $a2
+
+addi $sp, $sp, -4 # abre um espaço na stack para guardar o endereço que está em $a0
+sw $a0, 0($sp) # guarda na pilha o endereço da string desti na pilha
+
+la $a0,($a1)  # carrega o endereço que está em $a1 para o $a0 porquê a função strlen conta os caracteres do $a0
+
+jal strlen # chama a função strlen
+
+move $t1,$v0 # move o resultado da função para $t1
+
+bgt $a2,$t1,erro # Se a quantidade de caracteres escolhidos pelo usuário for maior que a string origem, dará erro.
+
+lw $a0, 0($sp) # recupera o endereço da string destino da pilha.
+
+addi $sp, $sp, 4 # Restaura a pilha.
+
+loop4:
+
+lb $t0, 0($a1) # Carrega o caracter (src) que está em $a1
+
+sb $t0, 0($a0) # copia o caracter para (dest) a string destino que está em $a0
+
+addi $a1,$a1,1 # Soma 1 ao endereço da string (src) que está em $a1
+addi $a0,$a0,1 # Soma 1 ao endereço da string (dest) que está em $a0
+addi $s0,$s0,1 # Soma 1 ao $s0 que está agindo como um contador
+
+beq $a2, $s0, end4 # Se o caracter que está em $t0 não for o \0, ou seja, 0 ele continua concatenando.
+j loop4
+
+end4:
+sub $a0,$a0,$s0 # subtraindo $a0 que contem o endereço da string de $t0 para voltar para o início dela
+la $v0,($a0) # carregando o endereço do primeiro caracter da string para retornar.
+
+lw $ra, 0($sp)  #
+lw $t1, 4($sp)  #
+lw $t0, 8($sp)  # Restaura os valores de $t1,$t0 e $s0 que foram copiados para a pilha.
+lw $s0, 12($sp) # 
+addi $sp, $sp, 16 # Restaur ao ponteiro da pilha para o lugar original.
+
+jr $ra # Volta para quem chamou.
+
+erro: # erro caso o usuário peça para copiar mais caracteres do que existe na string origem
+la $a0,msgErro # carregando a mensagem de erro
+li $v0,4 # código para imprimir uma string
+syscall
+
+li $v0,10 # código para encerrar o programa
+syscall
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
